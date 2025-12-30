1b84637ba3ae104797760e898ac568eb64d5a71d
vijaypalla <vijaypalla222@gmail.com>
2025-12-30 22:19:15 +0530

    Add files via upload

---
+++ week1/Qst-1.py
@@ -0,0 +1,11 @@
+# level 1_question 1
+# basic programme of creating variables of different data types 
+
+Name ="Vijay Kumar"  # string data 
+age = 18   #int data type 
+height = 167.67 # float  data type
+are_you_student = True # boolean data type 
+print("My name is " + Name )
+print("I am " + str(age) +" years old ")
+print("I am "+ str(height) + " cm tall")
+print("are_you_student ? " + str(are_you_student))
---
+++ week1/Qst-2.py
@@ -0,0 +1,5 @@
+# level 1_question 2
+year = input(" Enter your birth year: " )   # user birth year as string
+int_year = int(year)   # user birth as int
+age = 2025 - int_year   #calculating age in 2025
+print(f"you will be {age} years old in 2025!")
