87a23d06c51271359135c2ee1d345e7ec358f24e
vijaypalla <vijaypalla222@gmail.com>
2025-12-30 22:20:48 +0530

    Update Qst-2.py

--- week1/Qst-2.py
+++ week1/Qst-2.py
@@ -1,5 +1,6 @@
 # level 1_question 2
 year = input(" Enter your birth year: " )   # user birth year as string
-int_year = int(year)   # user birth as int
+int_year = int(year)   # user birth year as int
 age = 2025 - int_year   #calculating age in 2025
 print(f"you will be {age} years old in 2025!")
+
