47add75c7b5a198827fba08b956c5ab4fba8da47
vijaypalla <vijaypalla222@gmail.com>
2025-12-31 22:14:41 +0530

    Add files via upload

---
+++ week1/Qst-8.py
@@ -0,0 +1,13 @@
+# Creating a traffic signal simulator
+Colour=input("Enter your colour : ")
+if Colour == "red" :
+    print("STOP")
+else:
+    if Colour == "yellow" :
+        print("Prepare to stop")
+    else:
+        if Colour == "green" :
+            print("You can go")
+        else:
+            print("Wrong input")
+    
