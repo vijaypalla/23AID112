6a21ca37ed16c1998e44820ba6258f4c015bded0
vijaypalla <vijaypalla222@gmail.com>
2025-12-31 22:02:07 +0530

    Add files via upload

---
+++ week1/Qst-7.py
@@ -0,0 +1,8 @@
+Number = int(input(" Enter a Number : "))
+if Number > 0 :
+    print("Your Number is positive")
+else:
+    if Number < 0 :
+        print("Your Number is negative")
+    else:
+        print("Your Number is zero")
