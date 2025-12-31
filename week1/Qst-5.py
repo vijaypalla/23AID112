89b6ac311e3f5c93103cac13d58e6abc6986da84
vijaypalla <vijaypalla222@gmail.com>
2025-12-31 21:42:14 +0530

    Add files via upload

---
+++ week1/Qst-5.py
@@ -0,0 +1,21 @@
+Num1=input("Enter your first number : ")
+Num2=input("Enter your second number : ")
+Int_Num1=int(Num1)  # convert sting to integer
+Int_Num2=int(Num2)  # convert sting to integer
+# sum
+sum = Int_Num1 + Int_Num2
+print(f"sum is : {sum}")
+# difference
+diff = Int_Num1 - Int_Num2
+print(f"difference is : {diff}")
+#product
+product = Int_Num1*Int_Num2
+print(f"product is : {product}")
+if Int_Num1>Int_Num2 :
+    print(f"maximum number is : {Int_Num1}")
+else:   
+  if Int_Num1<Int_Num2 :
+    print(f"maximum nnumber is :{Int_Num2}")
+  else:
+    print(f"They are equal")
+    
