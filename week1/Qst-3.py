c70fbac2604a94c7bfd31dfd2beca76e6757dc8d
vijaypalla <vijaypalla222@gmail.com>
2025-12-31 19:07:12 +0530

    Update Qst-3.py

--- week1/Qst-3.py
+++ week1/Qst-3.py
@@ -1,5 +1,6 @@
 price = 599.99 # assigning price value (Float data type)
 Discount= 0.15 # discount given (float data type)
-Final_price=price- (price * Discount)
+Final_price=price - (price * Discount)
 Round_price= round(Final_price,2) # rounding final price to 2 decimals 
+
 print(Round_price)
