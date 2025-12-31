62a10a2d29f29f0906dcf89d72baf84eb1ae8b8e
vijaypalla <vijaypalla222@gmail.com>
2025-12-31 22:23:02 +0530

    Add files via upload

---
+++ week1/Qst-9.py
@@ -0,0 +1,7 @@
+# scores of exam
+exam1_score = int(input("Enter your first exam score : "))
+exam2_score = int(input("Enter your second exam score : "))
+if exam1_score>50 and exam2_score>50 :
+    print("You passed")
+else:
+    print("You need to retake some exams")
